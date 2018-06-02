05bb629673e66244a819c1f5995a4c453ff35ba0
axtrace <sofinma@ya.ru>
2018-06-02 11:52:35 +0300

    add tests

--- text_transliter.py
+++ text_transliter.py
@@ -4,21 +4,23 @@ from langdetect import detect
 
 
 class TextTransliter(object):
+    # convert text from one alphabet to other.
     _input_text = ''
     _output_text = ''
 
-    def __init__(self, input_text=''):
+    def __init__(self, input_text='', input_lang=''):
         try:
             self._input_text = input_text
-            self._transliterate()
+            self._transliterate(input_lang)
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
 
-    def _transliterate(self):
+    def _transliterate(self, input_lang):
         # convert from russian to translit
         try:
-            input_lang = detect(self._input_text)
+            if input_lang == '':
+                input_lang = detect(self._input_text)
             if input_lang not in get_available_language_codes():
                 input_lang = 'ru'
             self._output_text = translit(self._input_text, input_lang, reversed=True)
