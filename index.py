ecacd535c20bb825b3211e9964fd4fc670c4c7f3
axtrace <4077607+axtrace@users.noreply.github.com>
2025-02-23 18:13:23 +0300

    Created class s3Adapter

--- index.py
+++ index.py
@@ -3,17 +3,14 @@ import telebot
 import os
 import boto3
 import requests
+import s3_adapter
 
 # Получаем токен бота из переменных окружения
 token = os.environ['TEST_TOKEN']
 
 bot = telebot.TeleBot(token)
 
-s3 = boto3.client('s3',
-    endpoint_url='https://storage.yandexcloud.net',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
-)
+s3a = s3_adapter.s3Adapter()
 
 def handler(event, context):
     message = telebot.types.Update.de_json(event['body'])
@@ -27,32 +24,31 @@ def handler(event, context):
 def handle_document(message):
     try:
         file_info = bot.get_file(message.document.file_id)
-        file_url = bot.get_file_url(file_info.file_id)  # <-- Используем метод telebot
+        file_url = bot.get_file_url(file_info.file_id)
 
         response = requests.get(file_url)
+
         if response.ok:
-            s3.put_object(
-                Bucket=os.environ['BUCKET_NAME'],
-                Key=message.document.file_name,
-                Body=response.content,
-                ContentType=message.document.mime_type
-            )
+            filename = message.document.file_name
+            mime_type = message.document.mime_type
+            body = response.content
+
+            s3a.put_object(filename, body, mime_type)
+            obj = s3a.get_object(filename, False)
+
             bot.reply_to(message, "✅ Файл успешно загружен в S3")
+            bot.reply_to(message, obj )
         else:
             bot.reply_to(message, f"❌ Ошибка {response.status_code} при загрузке файла")
 
     except Exception as e:
         bot.reply_to(message, f"⚠️ Ошибка: {str(e)}")
-        logger.error(f"File processing error: {e}")
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    try:
+        chat_id = message.chat.id
+        bot.reply_to(message, "privet-privet")
 
-    # Получаем идентификатор чата
-    chat_id = message.chat.id
-
-    # Отправляем ответ "privet"
-    bot.reply_to(message, "privet-privet")
-
-
-
+    except Exception as e:
+        bot.reply_to(message, f"⚠️ Ошибка: {str(e)}")
---
+++ s3_adapter.py
@@ -0,0 +1,51 @@
+import boto3
+import os
+
+class s3Adapter:
+    """connection to Object Storage"""
+
+    def __init__(self):
+        self._s3_ = self._init_s3_client_()
+        self._bucket_ = os.environ['BUCKET_NAME']
+
+    def _init_s3_client_(self):
+        return boto3.client(
+            endpoint_url='https://storage.yandexcloud.net',
+            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+        )
+
+    def put_object(self, filename, body, mime_type):
+        try:
+            _s3_.put_object(
+                Bucket=self._bucket_,
+                Key=filename,
+                Body=body,
+                ContentType=mime_type
+            )
+            return 0
+
+        except Exception as e:
+            return None
+
+    def get_object(self, filename, decode: bool = True) -> bytes | str | None:
+        """
+        Читает файл из Object Storage
+
+        :param bucket: Имя бакета
+        :param key: Ключ объекта (путь к файлу)
+        :param decode: Декодировать в строку
+        :return: Содержимое файла или None при ошибке
+        """
+        try:
+            response = self._s3_.get_object(Bucket=bucket, Key=filename)
+
+            if decode:
+                return response['Body'].read().decode('utf-8')
+            return response['Body'].read()
+
+        except s3.exceptions.NoSuchKey:
+            print(f"File {key} not found in bucket {bucket}")
+        except Exception as e:
+            print(f"Error accessing S3: {str(e)}")
+        return None
