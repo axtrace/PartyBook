c3b3f38438c49f38f1fc488aced3ffe323f25aca
axtrace <4077607+axtrace@users.noreply.github.com>
2025-09-10 09:41:59 +0300

    Обновлены импорты в app/index.py для использования относительных путей, что улучшает структуру проекта и предотвращает возможные конфликты с путями.

--- app/index.py
+++ app/index.py
@@ -5,9 +5,9 @@ import boto3
 import requests
 from . import config
 from . import s3_adapter
-from book_reader import BookReader
-from books_library import BooksLibrary
-from models import User, Book
+from .book_reader import BookReader
+from .books_library import BooksLibrary
+from .models import User, Book
 
 # Получаем токен бота из переменных окружения (для тестирования или для продакшена)
 token = os.environ['TOKEN']
