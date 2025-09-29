8b4342fa5ad8da820555ba6532e1b1fd62dc54c6
axtrace <4077607+axtrace@users.noreply.github.com>
2025-09-29 12:49:02 +0300

    Исправлен метод транслитерации имени файла в классе FileExtractor для использования внутреннего метода класса.

--- app/file_extractor.py
+++ app/file_extractor.py
@@ -13,7 +13,7 @@ class FileExtractor(object):
         # get file and filename which have been sent by user in bot
         file_info = telebot.get_file(message.document.file_id)
         downloaded_file = telebot.download_file(file_info.file_path)
-        filename = TextTransliter(message.document.file_name).translite_text()
+        filename = self.tt.translite_text(message.document.file_name)
         return downloaded_file, filename
 
     def local_save_file(self, telebot, message, download_path):
